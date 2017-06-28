d97e9d2c31890e838b8030dfadb64f5595d63724
pkindalov <panayotkindalov@gmail.com>
2017-06-28 21:35:07 +0300

    Litle changes in div tag in App.js

--- to-do-flux-demo/src/App.js
+++ to-do-flux-demo/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import TodoList from './components/TodoList'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App' />
+      <div className='App'>
+        <h1>My To Do List</h1>
+        <TodoList />
+      </div>
     )
   }
 }
