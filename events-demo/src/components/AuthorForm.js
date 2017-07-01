af55d3f00366688e1f6b4c6a9b4b84831a6a4bff
pkindalov <panayotkindalov@gmail.com>
2017-07-01 13:42:44 +0300

    Updaded AuthorForm.js - Added new created component input

--- events-demo/src/components/AuthorForm.js
+++ events-demo/src/components/AuthorForm.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react'
+import Input from './common/input'
 
 class AuthorForm extends Component {
   render () {
     return (
       <form>
-        <label htmlFor='firstName'>First Name:</label>
-        <input
-          type='text'
-          name='firstName'
-          id='firstName'
-          onChange={this.props.onChange}
-          value={this.props.author.firstName} /><br />
-        <label htmlFor='lastName'>Last Name:</label>
-        <input
-          type='text'
-          name='lastName'
-          id='lastName'
-          onChange={this.props.onChange}
-          value={this.props.author.lastName} /><br />
+        <Input name='firstName'
+          placeholder='First Name'
+          value={this.props.author.firstName}
+          errors={this.props.errors.firstName}
+          onChange={this.props.onChange} />
+        <br />
+        <Input name='lastName'
+          placeholder='Last Name'
+          value={this.props.author.lastName}
+          errors={this.props.errors.lastName}
+          onChange={this.props.onChange} />
         <input type='submit' value='Add Author' onClick={this.props.onSave} />
+
       </form>
 
       // <form>
