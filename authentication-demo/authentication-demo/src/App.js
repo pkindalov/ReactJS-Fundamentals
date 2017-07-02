ce37e82cd8c6912edb32c118ef6ebb1899fd2e8b
pkindalov <panayotkindalov@gmail.com>
2017-07-03 00:42:58 +0300

    Updated App.js file. Added routing

--- authentication-demo/authentication-demo/src/App.js
+++ authentication-demo/authentication-demo/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
+import {Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import RegisterPage from './users/RegisterPage'
+import AccountPage from './users/AccountPage'
+import PrivateRoute from './components/common/PrivateRoute'
+import LogoutPage from './users/LogoutPage'
+import LoginPage from './users/LoginPage'
 
 class App extends Component {
   render () {
     return (
-      <div className='App' />
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route path='/register' component={RegisterPage} />
+          <Route path='/login' component={LoginPage} />
+          <PrivateRoute path='/account' component={AccountPage} />
+          <PrivateRoute path='/logout' component={LogoutPage} />
+        </Switch>
+      </div>
     )
   }
 }
