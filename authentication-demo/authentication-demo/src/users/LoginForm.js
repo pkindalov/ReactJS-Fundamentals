390d0dcf196951968764a59e9bc05746245e4115
pkindalov <panayotkindalov@gmail.com>
2017-07-03 00:37:34 +0300

    Created component LoginForm.js which is just form for login

---
+++ authentication-demo/authentication-demo/src/users/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const LoginForm = (props) => (
+  <form>
+    <div>{props.error}</div>
+    <label htmlFor='email'>Email: </label>
+    <input
+      type='email'
+      name='email'
+      placeholder='E-mail:'
+      value={props.user.email}
+      onChange={props.onChange} />
+    <br />
+    <label htmlFor='password'>Password: </label>
+    <input
+      type='password'
+      name='password'
+      placeholder='Password:'
+      value={props.user.password}
+      onChange={props.onChange} />
+    <br />
+
+    <input type='submit' onClick={props.onSave} />
+  </form>
+)
+
+export default LoginForm
