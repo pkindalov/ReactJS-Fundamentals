0e2cb35be4ee4aef7d2103e5eb10649b34850c23
pkindalov <panayotkindalov@gmail.com>
2017-07-07 12:11:51 +0300

    Added reusable input

--- authentication-demo/authentication-demo/src/users/RegisterForm.js
+++ authentication-demo/authentication-demo/src/users/RegisterForm.js
@@ -1,32 +1,23 @@
 import React from 'react'
+import Input from './input'
 
 const RegisterForm = (props) => (
   <form>
     <div>{props.error}</div>
     <label htmlFor='email'>Email: </label>
-    <input
-      type='email'
-      name='email'
-      placeholder='E-mail:'
-      value={props.user.email}
-      onChange={props.onChange} />
+    <Input type='email' name='email' placeholder='E-Mail:' value={props.user.email} onChange={props.onChange} />
+
     <br />
     <label htmlFor='password'>Password: </label>
-    <input
-      type='password'
-      name='password'
-      placeholder='Password:'
-      value={props.user.password}
-      onChange={props.onChange} />
+
+    <Input type='password' name='password' placeholder='Password' value={props.user.password} onChange={props.onChange} />
+
     <br />
     <label htmlFor='confirmPassword'>Confirm Password: </label>
-    <input
-      type='password'
-      name='confirmPassword'
-      placeholder='Password:'
-      value={props.user.confirmPassword}
-      onChange={props.onChange} />
+
+    <Input type='password' name='confirmPassword' placeholder='Confirm Password' value={props.user.confirmPassword} onChange={props.onChange} />
     <br />
+
     <input type='submit' onClick={props.onSave} />
   </form>
 )
